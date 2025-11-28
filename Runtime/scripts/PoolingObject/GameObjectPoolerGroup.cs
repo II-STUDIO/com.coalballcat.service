5848a51319487c8b994177192a93f9c330aa1982
Tan <watchaphon40225@gmail.com>
2025-11-28 14:42:25 +0700

    Update -> Add pooler group release

--- Runtime/scripts/PoolingObject/GameObjectPoolerGroup.cs
+++ Runtime/scripts/PoolingObject/GameObjectPoolerGroup.cs
@@ -7,12 +7,16 @@ namespace Coalballcat.Services
     public class GameObjectPoolerGroup : IDisposable
     {
         private readonly Dictionary<GameObject, GameObjectPooler> poolers;
-        private Transform parent;
+        private readonly Dictionary<GameObject, GameObject> prefabConnection;
+        private readonly List<GameObject> instanceTemp;
+        private readonly Transform parent;
         private int initialCapacity;
 
         public GameObjectPoolerGroup(int initialCapacity, Transform parent = null)
         {
             poolers = new Dictionary<GameObject, GameObjectPooler>();
+            prefabConnection = new Dictionary<GameObject, GameObject>();
+            instanceTemp = new List<GameObject>(256);
             this.parent = parent;
             this.initialCapacity = initialCapacity;
         }
@@ -43,43 +47,49 @@ namespace Coalballcat.Services
         public GameObject Pool(GameObject prefab)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool();
+            GameObject instance = pooler.Pool();
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, Transform parent)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(parent);
+            GameObject instance = pooler.Pool(parent);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position);
+            GameObject instance = pooler.Pool(position);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, in Quaternion rotation)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, rotation);
+            GameObject instance = pooler.Pool(position, rotation);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, Transform parent)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, parent);
+            GameObject instance = pooler.Pool(position, parent);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, in Quaternion rotation, Transform parent)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, rotation, parent);
+            GameObject instance = pooler.Pool(position,rotation,parent);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
         #endregion
 
@@ -88,45 +98,110 @@ namespace Coalballcat.Services
         public GameObject Pool(GameObject prefab, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(out isCreateNew);
+            GameObject instance = pooler.Pool(out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, Transform parent, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(parent, out isCreateNew);
+            GameObject instance = pooler.Pool(parent,out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, out isCreateNew);
+            GameObject instance = pooler.Pool(position,out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, in Quaternion rotation, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, rotation, out isCreateNew);
+            GameObject instance = pooler.Pool(position, rotation, out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, Transform parent, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, parent, out isCreateNew);
+            GameObject instance = pooler.Pool(position, parent, out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public GameObject Pool(GameObject prefab, in Vector3 position, in Quaternion rotation, Transform parent, out bool isCreateNew)
         {
             GameObjectPooler pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, rotation, parent, out isCreateNew);
+            GameObject instance = pooler.Pool(position, rotation, parent, out isCreateNew);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
         #endregion
+
+        public bool TryRelease(GameObject pool)
+        {
+            if(prefabConnection.TryGetValue(pool, out GameObject prefab))
+            {
+                GameObjectPooler pooler = GetPooler(prefab);
+                pooler.Release(pool);
+                prefabConnection.Remove(pool);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool TryReleasePooler(GameObject prefab)
+        {
+            if(poolers.TryGetValue(prefab, out GameObjectPooler pooler))
+            {
+                ReleasePrefabConnect(prefab);
+
+                pooler.Dispose();
+                poolers.Remove(prefab);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ReleasePrefabConnect(GameObject prefab)
+        {
+            foreach (var kv in prefabConnection)
+            {
+                if(kv.Value == prefab)
+                {
+                    instanceTemp.Add(kv.Key);
+                    break;
+                }
+            }
+
+            int count = instanceTemp.Count;
+
+            if (count == 0)
+                return;
+
+            for (int i = 0; i < count; i++)
+            {
+                GameObject instance = instanceTemp[i];
+                if (!instance)
+                    continue;
+
+                prefabConnection.Remove(instance);
+            }
+
+            instanceTemp.Clear();
+        }
 
         public void Clear()
         {
@@ -135,6 +210,8 @@ namespace Coalballcat.Services
             {
                 pooler.Clear();
             }
+
+            instanceTemp.Clear();
         }
 
         public void Dispose()
@@ -146,7 +223,8 @@ namespace Coalballcat.Services
             }
 
             poolers.Clear();
-            parent = null;
+            prefabConnection.Clear();
+            instanceTemp.Clear();
         }
     }
 }
--- Runtime/scripts/PoolingObject/MonoPoolerGroup.cs
+++ Runtime/scripts/PoolingObject/MonoPoolerGroup.cs
@@ -7,7 +7,9 @@ namespace Coalballcat.Services
     public class MonoPoolerGroup<T> : IDisposable where T : Component
     {
         private readonly Dictionary<T, MonoPooler<T>> poolers;
-        private Transform parent;
+        private readonly Dictionary<T, T> prefabConnection;
+        private readonly List<T> instanceTemp;
+        private readonly Transform parent;
         private int initialCapacity;
 
         public MonoPoolerGroup(int initialCapacity, Transform parent = null)
@@ -43,45 +45,111 @@ namespace Coalballcat.Services
         public T Pool(T prefab)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
-            return pooler.Pool();
+            T instance = pooler.Pool();
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public T Pool(T prefab, Transform parent)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
-            return pooler.Pool(parent);
+            T instance = pooler.Pool(parent);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public T Pool(T prefab, in Vector3 position)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
-            return pooler.Pool(position);
+            T instance = pooler.Pool(position);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public T Pool(T prefab, in Vector3 position, in Quaternion rotation)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, rotation);
+            T instance = pooler.Pool(position, rotation);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public T Pool(T prefab, in Vector3 position, Transform parent)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
-            return pooler.Pool(position, parent);
+            T instance = pooler.Pool(position, parent);
+            prefabConnection[instance] = prefab;
+            return instance;
         }
 
         public T Pool(T prefab, in Vector3 position, in Quaternion rotation, Transform parent)
         {
             MonoPooler<T> pooler = GetPooler(prefab);
-
+            T instance = pooler.Pool(position, rotation, parent);
+            prefabConnection[instance] = prefab;
             return pooler.Pool(position, rotation, parent);
         }
         #endregion
+
+
+        public bool TryRelease(T pool)
+        {
+            if (prefabConnection.TryGetValue(pool, out T prefab))
+            {
+                MonoPooler<T> pooler = GetPooler(prefab);
+                pooler.Release(pool);
+                prefabConnection.Remove(pool);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool TryReleasePooler(T prefab)
+        {
+            if (poolers.TryGetValue(prefab, out MonoPooler<T> pooler))
+            {
+                ReleasePrefabConnect(prefab);
+
+                pooler.Dispose();
+                poolers.Remove(prefab);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ReleasePrefabConnect(T prefab)
+        {
+            foreach (var kv in prefabConnection)
+            {
+                if (kv.Value == prefab)
+                {
+                    instanceTemp.Add(kv.Key);
+                    break;
+                }
+            }
+
+            int count = instanceTemp.Count;
+
+            if (count == 0)
+                return;
+
+            for (int i = 0; i < count; i++)
+            {
+                T instance = instanceTemp[i];
+                if (!instance)
+                    continue;
+
+                prefabConnection.Remove(instance);
+            }
+
+            instanceTemp.Clear();
+        }
 
         public void Clear()
         {
@@ -101,7 +169,6 @@ namespace Coalballcat.Services
             }
 
             poolers.Clear();
-            parent = null;
         }
     }
 }
